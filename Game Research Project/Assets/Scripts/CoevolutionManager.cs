78d9f193c756a01f421b0c42227f3e9d0a706cf8
Tony Wilson <57398501+TonyWilson-dev@users.noreply.github.com>
2023-04-24 00:11:52 +0100

    starting work on genes

--- Game Research Project/Assets/Scripts/CoevolutionManager.cs
+++ Game Research Project/Assets/Scripts/CoevolutionManager.cs
@@ -8,6 +8,9 @@ public class CoevolutionManager : MonoBehaviour
 {
 
     private byte[] _testGeneArray = new byte[6] { 1, 2, 3, 4, 5, 6 };
+    
+    
+    
 
     private GeneInfo _testGeneInfo = new GeneInfo(new byte[] {1,2,3,4,5,6}, "testGeneID"); 
 
@@ -29,4 +32,23 @@ public class CoevolutionManager : MonoBehaviour
         agent.Initialise(_testGeneArray);
 
     }
+
+    public void setGenesRandom(GeneInfo geneInfo)
+    {
+        for (int i = 0; i < geneInfo._geneArray.Length; i++)
+        {
+            geneInfo._geneArray[i] = (byte)Random.Range(1,10);
+        }
+    }
+
+    public void mutateGene(GeneInfo geneInfo)
+    {
+        for (int i = 0; i < geneInfo._geneArray.Length; i++)
+        {
+            int geneValue = (int)geneInfo._geneArray[i];
+            geneValue = (int)(geneValue + geneValue * 0.1);
+            geneInfo._geneArray[i] = (byte)geneValue;
+        }
+    }
+    
 }
